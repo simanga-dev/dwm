41bf7bad6a47375e7ae65609f2065b711702ea28
Hendry <h3ndry@users.noreply.github.com>
2022-08-19 19:16:38 +0200

    fixing zooming to the correct keymap

--- config.def.h
+++ config.def.h
@@ -37,7 +37,7 @@ static const Rule rules[] = {
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
 static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
-static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
+static const int lockfullscreen = 0; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
@@ -86,14 +86,13 @@ static Key keys[] = {
 	// { MODKEY,                       XK_d,      			ncnmaster,     				{.i = -1 } },
 	// { MODKEY,                       XK_h,      			setmfact,       			{.f = -0.05} },
 	// { MODKEY,                       XK_l,      			setmfact,       			{.f = +0.05} },
-	// { MODKEY,                       XK_Return, 			zoom,           			{0} },
+	{ MODKEY,                       XK_space, 				zoom,           			{0} },
 	{ MODKEY,                       XK_Tab,    				view,           			{0} },
 	{ MODKEY,             			XK_q,      				killclient,     			{0} },
 	{ MODKEY|ShiftMask,             XK_q,          			quit,           			{0} },
 	{ MODKEY,                       XK_t,      				setlayout,      			{.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      				setlayout,      			{.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      				setlayout,      			{.v = &layouts[2]} },
-	{ MODKEY,                       XK_space,  				setlayout,      			{0} },
 	{ MODKEY|ShiftMask,             XK_space,  				togglefloating, 			{0} },
 	{ MODKEY,                       XK_0,      				view,           			{.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      				tag,            			{.ui = ~0 } },
