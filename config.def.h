a77cef36f5edf077cbfaa5a701a8544df7591979
hendry <h3ndry@users.noreply.github.com>
2024-06-12 21:18:48 +0000

    fix(font): change font size

--- config.def.h
+++ config.def.h
@@ -6,8 +6,8 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "JetBrainsMono:size=12" };
-static const char dmenufont[]       = "JetBrainsMono:size=11";
+static const char *fonts[]          = { "JetBrainsMono:size=10" };
+static const char dmenufont[]       = "JetBrainsMono:size=10";
 static const char col_gray1[]       = "#21262d";
 static const char col_gray2[]       = "#21262d";
 static const char col_gray3[]       = "#a9b1d6";
